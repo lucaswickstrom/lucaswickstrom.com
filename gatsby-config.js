25cd2d6de0f5aa76168cfb03367e7b686806ce74
Lucas Wickström <lucaswickstrom@gmail.com>
2020-11-29 17:59:41 +0100

    Adds google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-emotion'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'G-W4GRB35MPB',
+      },
+    },
+  ],
 };
